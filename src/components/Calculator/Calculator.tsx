f2b4318232d9ff146fc706a3b6ed70ddf475be21
JPatrik <jokhel.roland@gmail.com>
2022-05-27 14:46:10 +0200

    feat(Calculator.tsx): add calculator display and buttons

---
+++ src/components/Calculator/Calculator.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+export default function Calculator() {
+    const [numbers, setNumbers] = useState('');
+    const operators = ['+', '-', '/', '*', '.'];
+    const digitBtns = Array.from({ length: 10 }).map((_, i) => {
+        return <button key={i}>{i}</button>;
+    });
+
+    return (
+        <div className='Calculator'>
+            <div className='Display'>{numbers === '' ? '0' : numbers}</div>
+            <div className='Operators'>
+                {operators.map((operator) => (
+                    <button key={operator}>{operator}</button>
+                ))}
+            </div>
+            <div className='Digits'>{digitBtns}</div>
+            <div className='FunctionalBtns'>
+                <button>=</button>
+                <button>Del</button>
+                <button>Save</button>
+                <button>Read</button>
+            </div>
+        </div>
+    );
+}
